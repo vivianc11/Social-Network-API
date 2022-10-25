ec2b789909d0fff4379f48219edfc3943a3f3679
Vivian Chen <vivian.chen1193@gmail.com>
2022-10-24 17:23:17 -0700

    created thought model

---
+++ models/Thought.js
@@ -0,0 +1,76 @@
+// IMPORTING
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment')
+
+// THOUGHT SCHEMA
+const thoughtSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+// REACTION SCHEMA
+const reactionSchema = new Schema (
+    {
+       reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+       },
+       reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+       },
+       username: {
+        type: String,
+        required: true,
+       },
+       createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+       },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+)
+
+
+// GET TOTAL FRIEND COUNT
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+})
+
+
+const Thought = model('Thought', thoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
